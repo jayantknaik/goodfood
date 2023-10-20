fa794083463d7307f20da8d8238bf3865dc6073d
jayantknaik <jayantnaik98@gmail.com>
2023-10-20 09:27:32 +0530

    7

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,6 +1,7 @@
-import { MENU_URL } from "../utils/constants";
+import { IMG_URL, MENU_URL } from "../utils/constants";
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
+import { IMG_URL } from "../utils/constants";
 
 const RestaurantMenu = () => {
 
@@ -21,16 +22,14 @@ const RestaurantMenu = () => {
 
   return (resInfo.length===0) ? <Shimmer /> : (
     <div className="menu">
-        <div className="menu__name">{ details.name }</div>
-        <div className="menu__cuisines">{ details.cuisines.join(", ") }</div>
-        <div className="menu__area">{ details.areaName }</div>
-        <div className="menu__rating">{ details.avgRating }</div>
-        <div className="menu__rating">&#8377;{ details.costForTwo / 100 }</div>
-        <ul>
-          <li>Biryani</li>
-          <li>Burgers</li>
-          <li>Diet Coke</li>
-        </ul>
+        <img className="menu__img" src={IMG_URL + details.cloudinaryImageId} alt="" />
+        <div className="menu__info">
+          <div className="menu__name">{ details.name }</div>
+          <div className="menu__cuisines">{ details.cuisines.join(", ") }</div>
+          <div className="menu__area">{ details.areaName }</div>
+          <div className="menu__rating">{ details.avgRating }</div>
+          <div className="menu__cost">&#8377;{ details.costForTwo / 100 }</div>
+        </div>
     </div>
   )
 }
