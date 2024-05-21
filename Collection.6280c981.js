4be7850109b4bb5237d1390c60ea7b54bca471d6
Jayant Kumar Naik <jayant.naik@magicbricks.com>
2024-05-21 16:32:27 +0530

    Updates

---
+++ Collection.6280c981.js
@@ -0,0 +1,3 @@
+var e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire028a;e.register("apD72",function(n,o){Object.defineProperty(n.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(n.exports,"default",{get:function(){return t},set:void 0,enumerable:!0,configurable:!0});var r=e("R1mjg"),t=()=>/*#__PURE__*/(0,r.jsx)(r.Fragment,{})});//# sourceMappingURL=Collection.6280c981.js.map
+
+//# sourceMappingURL=Collection.6280c981.js.map
--- GoodFood/Collection.e3d54d2c.js
+++
@@ -1,3 +0,0 @@
-var e;(e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire028a).register("8N796",function(n,o){Object.defineProperty(n.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(n.exports,"default",{get:function(){return t},set:void 0,enumerable:!0,configurable:!0});var r=e("45FY7"),t=function(){return/*#__PURE__*/(0,r.jsx)(r.Fragment,{})}});//# sourceMappingURL=Collection.e3d54d2c.js.map
-
-//# sourceMappingURL=Collection.e3d54d2c.js.map
