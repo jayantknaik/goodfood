1c31191c8fbc859e29308ee804c80cb7ae22b46f
Jayant Kumar Naik <jayant.naik@magicbricks.com>
2025-03-05 17:38:54 +0530

    server.js Updates

--- server/server.js
+++ server/server.js
@@ -13,8 +13,10 @@ const IMG_URL = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_
 app.use(cors());
 app.use(express.json());
 
-app.get("/create-checkout-session", (req, res) => {
-    res.send("Checkout session created.");
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("/*", (req,res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 })
 
 app.post("/create-checkout-session", async (req, res) => {
